4e03e2c36c1b4589812bf16a302167e2c61d5e52
Bui Anh Duy <aduy8774@gmail.com>
2025-02-12 14:09:39 +0700

    ...

---
+++ src/main/java/DAO/orderDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+/**
+ *
+ * @author Nguyen Phu Quy CE180789
+ */
+public class orderDAO {
+    int number;
+    
+}
